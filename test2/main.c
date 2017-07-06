e336a7088ecd3fa48adf5ec5cf0da786dbd66dec
liuqiuchen <2016968116@qq.com>
2017-07-06 11:25:39 +0800

    commit

---
+++ test2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include "xiangqi.c"
+//#include "sushu.c"
+#include "shuixianhua.c"
+
+int main()
+{
+    //xiangqi();
+    //sushu();
+    shuixianhua();
+
+    return 0;
+
+}
